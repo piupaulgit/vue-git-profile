04d2d4a39372a4a08b5fd2b32f992559104967e2
piupaulgit <contactpiu619@gmail.com>
2020-12-23 00:37:08 +0530

    api call for git username

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './style.scss';
@@ -12,6 +14,7 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
 
 const routes = [
   {
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,8 +1,36 @@
 <template>
-    <h1>This is Profile</h1>
+    <div>
+        <h1>This is Profile</h1>
+        <div v-if="hasError">
+            <h1>No Github profile found with username {{username}}</h1>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name:'Profile'
+    name:'Profile',
+    data() {
+        return {
+            username: this.$route.params.username,
+            userInfo: Object,
+            userConfig: Object,
+            reposInfo: Object,
+            hasError: false
+        }
+    },
+     created() {
+        this.axios
+        .get(`https://api.github.com/users/${this.username}`)
+        .then((res) => {
+            this.userInfo = res.data;
+            this.userConfig = res.config;
+            this.axios.get(`${this.userInfo.repos_url}`).then((repoRes) => {
+            this.reposInfo = repoRes.data;
+            this.hasError = false;
+            });
+        }).catch(() => {
+            this.hasError = true;
+        });
+    }
 }
 </script>
