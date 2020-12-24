226d4d7d683f01273251f5b975ff8fe6dcba2e35
piupaulgit <contactpiu619@gmail.com>
2020-12-24 11:39:04 +0530

    charts added

---
+++ src/components/Charts.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="chart">
+        <div class="chart_item">
+             <highcharts :options="chartOptions"></highcharts>
+        </div>
+        <div class="chart_item">
+             <highcharts :options="chartOptions"></highcharts>
+        </div>
+        <div class="chart_item">
+             <highcharts :options="chartOptions"></highcharts>
+        </div>
+        
+    </div>
+</template>
+<script>
+import {Chart} from 'highcharts-vue'
+export default {
+    name:"Charts",
+    components: {
+        highcharts: Chart 
+    },
+    data(){
+        return {
+             chartOptions: {
+                type: 'variablepie',
+                series: [{
+                data: [1,2,3] // sample data
+                }]
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/variables';
+.chart{
+    display: flex;
+    gap: 20px;
+    margin-top: -40px;
+    align-items: center;
+    justify-content: center;
+    .chart_item{
+        max-width: 30%;
+        flex: 0 0 30%;
+        background: $white_color;
+        border-radius: 5px;
+        overflow: hidden;
+        box-shadow: 2px 2px 2px red;
+    }
+}
+</style>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,6 +3,7 @@
         <div v-if="!hasError">
             <GithubIcon></GithubIcon>
             <Header v-bind:userInfo="userInfo"></Header>
+            <charts></charts>
             <h1>Github profile found with username {{username}}</h1>
         </div>
         <div v-if="hasError">
@@ -13,9 +14,11 @@
 <script>
 import GithubIcon from "../components/GithubIcon";
 import Header from "../components/Header";
+import GhPolyglot from 'gh-polyglot';
+import Charts from '../components/Charts.vue';
 export default {
     name:'Profile',
-    components: {GithubIcon, Header},
+    components: {GithubIcon, Header, Charts},
     data() {
         return {
             username: this.$route.params.username,
@@ -26,18 +29,24 @@ export default {
         }
     },
      created() {
-        this.axios
-        .get(`https://api.github.com/users/${this.username}`)
-        .then((res) => {
-            this.userInfo = res.data;
-            this.userConfig = res.config;
-            this.axios.get(`${this.userInfo.repos_url}`).then((repoRes) => {
-            this.reposInfo = repoRes.data;
-            this.hasError = false;
+         this.axios
+            .get(`https://api.github.com/users/${this.username}`)
+            .then((res) => {
+                this.userInfo = res.data;
+                this.userConfig = res.config;
+                this.axios.get(`${this.userInfo.repos_url}`).then((repoRes) => {
+                this.reposInfo = repoRes.data;
+                this.hasError = false;
+                });
+            }).catch(() => {
+                this.hasError = true;
             });
-        }).catch(() => {
-            this.hasError = true;
-        });
+             const me = new GhPolyglot(`${this.username}`);
+             me.userStats(function (err, stats) {
+                console.log(err || stats);
+            });
+    },
+    methods() {
     }
 }
 </script>
